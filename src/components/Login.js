af31c6ec5e43f4fe3e571809a447b09e866d3837
Brent Besselievre <brent@Brents-MacBook-Pro.local>
2021-09-17 09:16:09 -0700

    Login Form is working.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+
+const initialFormValues = {
+  username: '',
+  password: '',
+}
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
 
+  //state
+  const [formValues, setFormValues] = useState(initialFormValues)
+
   const error = "";
   //replace with error state
+
+  //helper functions
+  const changeHandler = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("Clicked")
+  }
 
   return (
     <div>
       <h1>Welcome to the Bubble App!</h1>
       <div data-testid="loginForm" className="login-form">
         <h2>Build login form here</h2>
+        <form>
+          
+          <label>
+            <input 
+              type='text'
+              name='username'
+              placeholder='Username'
+              id='username'
+              value={formValues.username}
+              onChange={changeHandler}
+            /> 
+          </label>
+
+          <label>
+            <input 
+              type='password'
+              placeholder='Password'
+              name='password'
+              id='password'
+              value={formValues.password}
+              onChange={changeHandler}
+            /> 
+          </label>
+
+        </form>
+
+        <button id='submit' onClick={submitHandler}>Login</button>
       </div>
 
       <p id="error" className="error">{error}</p>
